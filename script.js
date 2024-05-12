fb67dc036ed66f22715a81bfc71017c150f1d60a
flarnhess <152910344+flarnhess@users.noreply.github.com>
2024-05-13 00:30:28 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+//var reismciks = [
+//    "https://i.imgur.com/vyTqtmr.gif",
+//    "https://i.imgur.com/Z2APgEt.gif",
+//    "https://i.imgur.com/YWhc4iA.gif",
+//    "https://i.imgur.com/4f7fjPE.gif",
+//    "https://i.imgur.com/zHtMSAg.gif",
+//    "https://i.imgur.com/1U8eRI6.jpeg",
+//    ]
+//    document.body.style.backgroundImage = "url(" + reismciks[Math.floor(Math.random() * reismciks.length)] + ")"
+//
+// if i didnt choose kira yoshikage background as a background this code will randomly select one of this gif links and it as a background
+
+function ginga () {
+    alert("Aleyküm selam kardeşim!")
+}
+
+function play() {
+    var audio = document.getElementById("audio");
+    audio.play();
+  }
+
+function play1() {
+    var audio1 = document.getElementById("sescik");
+    audio1.play();
+}
+
+function myfunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myfunction1() {
+    var x = document.getElementById("myDIV1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function gunga() {
+    var x = document.getElementById("gungas");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function hover(element) {
+    element.setAttribute('src', 'assets/github2.png');
+  }
+  
+  function unhover(element) {
+    element.setAttribute('src', 'assets/github11.png');
+  }
+
+  function hoverme(element) {
+    element.setAttribute('src', 'assets/discord2.png');
+  }
+  
+  function unhoverme(element) {
+    element.setAttribute('src', 'assets/discord11.png');
+  }
+//sa as 
+function degisbeniz() {
+  document.body.style.backgroundImage = 'url(assets/backg.jpeg)';  
+}
